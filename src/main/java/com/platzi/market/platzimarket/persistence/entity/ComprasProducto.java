91a3c50391346e25f4700f425cfdcda4a613ca29
CristianX <thecristianx@hotmail.com>
2022-09-05 20:43:00 -0500

    Añadiendo entitys y ComprasProducto y ComprasProductoPK para claves primarias complejas

---
+++ src/main/java/com/platzi/market/platzimarket/persistence/entity/ComprasProducto.java
@@ -0,0 +1,54 @@
+package com.platzi.market.platzimarket.persistence.entity;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "compras_productos")
+public class ComprasProducto {
+    @EmbeddedId
+    private ComprasProductoPK id;
+    
+    private Integer cantidad;
+    private Double total;
+    private Boolean estado;
+
+
+    public ComprasProductoPK getId() {
+        return this.id;
+    }
+
+    public void setId(ComprasProductoPK id) {
+        this.id = id;
+    }
+
+    public Integer getCantidad() {
+        return this.cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Double getTotal() {
+        return this.total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    public Boolean isEstado() {
+        return this.estado;
+    }
+
+    public Boolean getEstado() {
+        return this.estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+
+}
